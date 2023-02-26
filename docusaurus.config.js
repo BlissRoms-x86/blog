95861350e8065aebcd4731dd30135674e83ad211
Jackeagle <jackeagle102@gmail.com>
2023-02-26 15:40:50 +0530

    config: Update Copyright footer
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} BlissLabs, All rights reserved.`,
       },
       prism: {
         theme: lightCodeTheme,
