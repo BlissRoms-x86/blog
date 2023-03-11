ddf4b1f5860ed6679c407f9ac79b2019bc50dc68
electrikjesus <electrikjesus@users.noreply.github.com>
2023-03-11 18:55:12 +0000

    deploy: 9b5f0e21c6f0eec121b5305e3c799f278b97bb4e

---
+++ assets/js/814f3328.350a46d1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog_1=self.webpackChunkblog_1||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"2023/what-can-open-source-do-for-you","permalink":"/2023/what-can-open-source-do-for-you"}]}')}}]);
